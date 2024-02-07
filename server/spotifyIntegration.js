890050ae6851e6391676379d14e0cff63fa6633b
Maxym Charpentier <maxym.charpentier@etu.univ-orleans.fr>
2024-02-07 20:13:36 +0100

    Refactor artist and album import logic

--- server/spotifyIntegration.js
+++ server/spotifyIntegration.js
@@ -54,21 +54,30 @@ async function fetchSpotifyData(endpoint) {
 
 
 async function importArtistsAndAlbums() {
-    // Token d'accès Spotify
-    const accessToken = await getSpotifyAccessToken();
-
     // Récupérer les artistes
-    const artistsData = await fetchSpotifyData('search?q=genre:%22rock%22&type=artist&limit=10');
+    
+    const artistsData = await fetchSpotifyData('search?q=a&type=artist&limit=50');
+
+
+    const uniqueArtists = [];
+    const seenArtists = new Set();
     for (const artist of artistsData.artists.items) {
+        const lowercaseTrimmedName = artist.name.toLowerCase().trim();
+        if (!seenArtists.has(lowercaseTrimmedName)) {
+            uniqueArtists.push(artist);
+            seenArtists.add(lowercaseTrimmedName);
+        }
+    }
+    for (const artist of uniqueArtists) {
         // Insérer chaque artiste dans la base de données
         const artistStmt = db.prepare('INSERT INTO artists (name, genre) VALUES (?, ?)');
         const artistInfo = artistStmt.run(artist.name, artist.genres[0] || 'Non spécifié');
 
-        // Récupérer les IDs d'albums pour l'artiste
-        const albumsData = await fetchSpotifyData(`artists/${artist.id}/albums?include_groups=album,single&market=US`);
+        // Récupérer les IDs d'albums pour l'artiste avec une limite de 5 albums
+        const albumsData = await fetchSpotifyData(`artists/${artist.id}/albums?include_groups=album,single&market=US&limit=5`);
         const albumIds = albumsData.items.map(album => album.id).join(',');
 
-        // Récupérer les données des albums en utilisant les IDs récupérés
+        // Récupérer les détails des albums en utilisant les IDs récupérés
         const albumsDetails = await fetchSpotifyData(`albums?ids=${albumIds}&market=US`);
         for (const album of albumsDetails.albums) {
             // Insérer chaque album dans la base de données
@@ -78,8 +87,9 @@ async function importArtistsAndAlbums() {
             const albumId = db.prepare('SELECT last_insert_rowid() AS id').get().id;
             const albumInfo = { id: albumId }; // Stockez les informations sur l'album dans la variable albumInfo
 
-            // Insérer les pistes de l'album
-            for (const track of album.tracks.items) {
+            // Insérer les pistes de l'album avec une limite de 10 pistes par album
+            const tracksData = await fetchSpotifyData(`albums/${album.id}/tracks?limit=10&market=US`);
+            for (const track of tracksData.items) {
                 const trackStmt = db.prepare('INSERT INTO musics (title, album_id) VALUES (?, ?)');
                 trackStmt.run(track.name, albumInfo.id);
             }
